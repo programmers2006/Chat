f75c978c8a8b580a42a6bef1d4b2da60334f01e0
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-11 15:11:45 +0500

    main

--- chat/views.py
+++ chat/views.py
@@ -7,7 +7,6 @@ def home(request):
     return render(request, "pages/home.html")
 
 def api():
-<<<<<<< HEAD
     #    while True:
             with open('example.txt', 'w', encoding='utf-8') as my_file:
                 r = requests.get("https://furn-master.herokuapp.com/api/carousel")
@@ -17,12 +16,5 @@ def api():
 
         # file = open('templates/pages/home.html', 'w')
         # file.write(res)
-=======
-    while True:
-        r = requests.get("https://furn-master.herokuapp.com/api/carousel/")
-        res = r.json()
-        file = open('templates/pages/home.html', 'w')
-        file.write(str(res))
->>>>>>> fb631bcf5338a4f5c424fd0c149fdb3cb1d6ce1e
-
+        
 api()
