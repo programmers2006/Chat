4dac3fe3fc9f3a453f1ed9dab99b6d623d1abbf3
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-10-11 15:09:37 +0500

    simple changes

--- chat/views.py
+++ chat/views.py
@@ -7,9 +7,9 @@ def home(request):
     return render(request, "pages/home.html")
 
 def api():
-       while True:
+    #    while True:
             with open('example.txt', 'w', encoding='utf-8') as my_file:
-                r = requests.get("https://furn-master.herokuapp.com/api/carousel/1")
+                r = requests.get("https://furn-master.herokuapp.com/api/carousel")
                 res = r.json()
                 my_dict = res
                 my_file.write(json.dumps(my_dict))
