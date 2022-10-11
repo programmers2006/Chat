92e12c7c353e5f7f7e4d410f11d05724bb149894
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-10-11 14:43:43 +0500

    get api

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests
 
 
 def home(request):
-    return render(request, "pages/home.html")
+    r = requests.get("https://furn-master.herokuapp.com/api/carousel/1")
+    res = r.json()
+    context ={
+        "api":res
+    }
+    return render(request, "pages/home.html", context)
+
