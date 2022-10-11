a3fac0396b831096c76f23704564e0185d995b24
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-10-11 15:06:04 +0500

    bugs fixed

--- chat/views.py
+++ chat/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 import requests
-
+import json
 
 def home(request):
 
     return render(request, "pages/home.html")
 
 def api():
-    while True:
-        r = requests.get("https://furn-master.herokuapp.com/api/carousel/")
-        res = r.json()
-        file = open('templates/pages/home.html', 'w')
-        file.write(str(res))
+       while True:
+            with open('example.txt', 'w', encoding='utf-8') as my_file:
+                r = requests.get("https://furn-master.herokuapp.com/api/carousel/1")
+                res = r.json()
+                my_dict = res
+                my_file.write(json.dumps(my_dict))
+
+        # file = open('templates/pages/home.html', 'w')
+        # file.write(res)
 
 api()
